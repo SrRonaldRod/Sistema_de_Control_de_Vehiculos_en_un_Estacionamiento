72fd12b41991b13984a2f08f9b1532151272c177
marvelvscampcom0986-pixel <marvelvscampcom0986@gmail.com>
2025-12-07 22:48:55 -0400

    Agregar Integrante - Jaiverson Infante C.I: 31.797.621

--- proyecto.c
+++ proyecto.c
@@ -3,5 +3,6 @@ int main() {
     printf("Integrantes:\n");
 printf("Ronald Rodriguez C.I: 31.762.090\n");
 printf ("Manuel Rodríguez C.I: 31.657.231\n");
+printf ("Jaiverson Infante C.I: 31.797.621\n");
     return 0;
 }   
