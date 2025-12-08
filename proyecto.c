bb4c7a0e8ddf49704db496a7d63239c71ee1aea1
David45003 <davidalejandrotorres203@gmail.com>
2025-12-07 22:56:06 -0400

    Agregar Integrante - David Torres C.I: 31.762.289

--- proyecto.c
+++ proyecto.c
@@ -5,5 +5,6 @@ printf("Ronald Rodriguez C.I: 31.762.090\n");
 printf ("Manuel Rodríguez C.I: 31.657.231\n");
 printf ("Jaiverson Infante C.I: 31.797.621\n");
 printf ("Alexander Sánchez C.I: 31.644.118\n");
+printf ("David Torres C.I:31.762.289\n");
     return 0;
 }   
