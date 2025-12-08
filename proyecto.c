501233c924d104f95516ed74a0baa28faa191750
rdaxx7 <rdaniel.unefa@gmail.com>
2025-12-07 23:22:04 -0400

    Agregar Integrante - Alexander Rodríguez C.I: 31.797.479

--- proyecto.c
+++ proyecto.c
@@ -6,5 +6,6 @@ printf ("Manuel Rodríguez C.I: 31.657.231\n");
 printf ("Jaiverson Infante C.I: 31.797.621\n");
 printf ("Alexander Sánchez C.I: 31.644.118\n");
 printf ("David Torres C.I:31.762.289\n");
+printf ("Alexander Rodríguez C.I: 31.797.479\n");
     return 0;
 }   
